7cb6ecdf423aa0b88ef1ab1b3285dfe24d81ea4a
JY8752 <j.y.8752@gmail.com>
2024-06-30 22:05:53 +0900

    update

--- demo/protovalidate/main.go
+++ demo/protovalidate/main.go
@@ -9,7 +9,29 @@ import (
 
 func main() {
 	msg := &hellov1.Hello{
-		Hello: "",
+		Hello:      "hello",
+		NumList:    []int32{5, 10, 15},
+		StringList: []string{"banana", "peach", "apple"},
+		UserList: []*hellov1.Hello_User{
+			{
+				Id:      1,
+				Name:    "user1",
+				Age:     18,
+				Country: "Japan",
+			},
+			{
+				Id:      2,
+				Name:    "user2",
+				Age:     20,
+				Country: "America",
+			},
+			{
+				Id:      3,
+				Name:    "user3",
+				Age:     20,
+				Country: "Japan",
+			},
+		},
 	}
 
 	v, err := protovalidate.New()
